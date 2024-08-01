e6e9af05511fbcc72c7043622e76cc6b8691a3c2
highinksy <piyush@pilot-tech.co>
2024-08-01 09:44:53 +0530

    reactnativeremove

--- apps/rn/src/components/TestResultDisplay.js
+++ apps/rn/src/components/TestResultDisplay.js
@@ -1,76 +1,76 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
-import PdfView from 'react-native-pdf';
+// import React, { Component } from 'react';
+// import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+// import PdfView from 'react-native-pdf';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e6e6e6',
-  },
-  topContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  text: {
-    flex: 1,
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  pdf: {
-    flex: 1,
-    paddingHorizontal: 10,
-    backgroundColor: 'transparent',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#e6e6e6',
+//   },
+//   topContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//   },
+//   text: {
+//     flex: 1,
+//     fontSize: 20,
+//     textAlign: 'center',
+//   },
+//   pdf: {
+//     flex: 1,
+//     paddingHorizontal: 10,
+//     backgroundColor: 'transparent',
+//   },
+// });
 
-export default class TestResultDisplay extends Component {
-  state = {
-    page: undefined,
-    numberOfPages: undefined,
-    filePath: undefined,
-  };
+// export default class TestResultDisplay extends Component {
+//   state = {
+//     page: undefined,
+//     numberOfPages: undefined,
+//     filePath: undefined,
+//   };
 
-  handleLoadComplete = (numberOfPages, filePath) => {
-    this.setState({ filePath });
-  };
+//   handleLoadComplete = (numberOfPages, filePath) => {
+//     this.setState({ filePath });
+//   };
 
-  handlePageChanged = (page, numberOfPages) => {
-    this.setState({ page, numberOfPages });
-  };
+//   handlePageChanged = (page, numberOfPages) => {
+//     this.setState({ page, numberOfPages });
+//   };
 
-  handleError = (e) => {
-    console.warn('Failed to render PDF', e);
-  };
+//   handleError = (e) => {
+//     console.warn('Failed to render PDF', e);
+//   };
 
-  render() {
-    const { base64Pdf, runtimeSecs, onDone } = this.props;
-    const { page, numberOfPages, filePath } = this.state;
+//   render() {
+//     const { base64Pdf, runtimeSecs, onDone } = this.props;
+//     const { page, numberOfPages, filePath } = this.state;
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.topContainer}>
-          <View style={{ flex: 1 }}>
-            <Button title="Done" onPress={onDone} />
-          </View>
+//     return (
+//       <SafeAreaView style={styles.container}>
+//         <View style={styles.topContainer}>
+//           <View style={{ flex: 1 }}>
+//             <Button title="Done" onPress={onDone} />
+//           </View>
 
-          <Text style={styles.text}>
-            Page {page}/{numberOfPages}
-          </Text>
+//           <Text style={styles.text}>
+//             Page {page}/{numberOfPages}
+//           </Text>
 
-          <Text style={styles.text}>{runtimeSecs} secs</Text>
-        </View>
+//           <Text style={styles.text}>{runtimeSecs} secs</Text>
+//         </View>
 
-        {base64Pdf && (
-          <PdfView
-            style={styles.pdf}
-            source={{ uri: base64Pdf }}
-            onLoadComplete={this.handleLoadComplete}
-            onPageChanged={this.handlePageChanged}
-            onError={this.handleError}
-          />
-        )}
-      </SafeAreaView>
-    );
-  }
-}
+//         {base64Pdf && (
+//           <PdfView
+//             style={styles.pdf}
+//             source={{ uri: base64Pdf }}
+//             onLoadComplete={this.handleLoadComplete}
+//             onPageChanged={this.handlePageChanged}
+//             onError={this.handleError}
+//           />
+//         )}
+//       </SafeAreaView>
+//     );
+//   }
+// }
